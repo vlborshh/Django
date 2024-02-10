c50aca22cdafb43ed959e6c2ad1c848e1b8a699b
vlborshh <vlborshh@yandex.ru>
2024-02-10 10:50:24 +0300

    dz3

--- onlinestore/storeapp/urls.py
+++ onlinestore/storeapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('clients/', views.clients, name='clients'),
     path('product/', views.product, name='product'),
     path('order/', views.order, name='order'),
+    path('sorting_orders_date/<int:client_id>/<int:duration>', views.sorting_orders_date, name='sorting_orders_date'),
+    path('sorting_orders_date_week/<int:client_id>', views.sorting_orders_date_week, name='sorting_orders_date_week'),
+    path('sorting_orders_date_month/<int:client_id>', views.sorting_orders_date_month, name='sorting_orders_date_month'),
+    path('sorting_orders_date_year/<int:client_id>', views.sorting_orders_date_year, name='sorting_orders_date_year'),
 ]
--- onlinestore/storeapp/views.py
+++ onlinestore/storeapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from storeapp.models import Client, Product, Order
+from django.utils import timezone
 import logging
 
 
@@ -42,3 +43,65 @@ def order(request):
     order = Order.objects.all()
     return HttpResponse(order)
 
+# Продолжаем работать с товарами и заказами.
+#
+# Создайте шаблон, который выводит список заказанных клиентом товаров из всех его заказов с сортировкой по времени:
+# — за последние 7 дней (неделю)
+# — за последние 30 дней (месяц)
+# — за последние 365 дней (год)
+#
+# Товары в списке не должны повторятся.
+
+def sorting_orders_date_duration(client_id, duration, title):
+    products = []
+    client = get_object_or_404(Client, pk=client_id)
+    orders = Order.objects.filter(client_order=client,
+                                  date_order__gt=(timezone.now() - timezone.timedelta(days=duration))).all()
+    for order in orders:
+        for product in order.product_order.all():
+            products.append(product.name_product)
+    products = set(products)
+    context = {'title': title,
+               'client_id': client_id,
+               'items': products,
+               }
+    return context
+
+
+# — за последние 7 дней (неделю)
+def sorting_orders_date_week(request, client_id, duration=7):
+    title = 'Сортировка за неделю'
+    context = sorting_orders_date_duration(client_id, duration, title)
+    return render(request, 'storeapp/sorting_orders_date.html', context)
+
+
+# — за последние 30 дней (месяц)
+def sorting_orders_date_month(request, client_id, duration=30):
+    title = 'Сортировка за месяц'
+    context = sorting_orders_date_duration(client_id, duration, title)
+    return render(request, 'storeapp/sorting_orders_date.html', context)
+
+
+# — за последние 365 дней (год)
+def sorting_orders_date_year(request, client_id, duration=365):
+    title = 'Сортировка за год'
+    context = sorting_orders_date_duration(client_id, duration, title)
+    return render(request, 'storeapp/sorting_orders_date.html', context)
+
+
+# для любого периода
+def sorting_orders_date(request, client_id, duration):
+
+    day = ''
+    title = ''
+
+    if duration == 1:
+        day = 'день'
+    elif duration >= 2 and duration <= 4:
+        day= 'дня'
+    else:
+        day = 'дней'
+
+    title = f'Сортировка за период в {duration} {day}'
+    context = sorting_orders_date_duration(client_id, duration, title)
+    return render(request, 'storeapp/sorting_orders_date.html', context)
